d1ebfc444c76b34c96b47f095100b7acba1a7c12
anup <anuppal602@gmail.com>
2021-10-20 12:47:50 +0530

    last2

--- src/Search.jsx
+++ src/Search.jsx
@@ -4,10 +4,11 @@ import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
 import { Button } from "@material-ui/core";
-
+import {useHistory} from 'react-router-dom'
 import PeopleIcon from "@material-ui/icons/People";
 
 function Search() {
+  const history = useHistory();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -30,7 +31,10 @@ function Search() {
         <PeopleIcon />
       </h2>
       <input min={0} defaultValue={2} type="number" />
-      <Button>Search Airbnb</Button>
+      <Button onClick={()=>
+      history.push("/search")
+      
+      }>Search Airbnb</Button>
     </div>
   );
 }
