a3e458619571b0b0266c91fecb50573d3be02cbb
anup <anuppal602@gmail.com>
2021-10-20 12:36:19 +0530

    last

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import "./Banner.css";
 import {Button } from '@material-ui/core'
-
+import {useHistory} from 'react-router-dom'
 import Search from "./Search";
 
 
 function Banner() {
+  const history = useHistory();
     const [showSearch, setShowSearch] = useState (false);
 
 
@@ -25,7 +26,9 @@ function Banner() {
         <h5>
             Plan a different kind of gateway to uncover the hidden gems near you.
         </h5>
-        <Button variant="outlined">
+        <Button variant="outlined"onClick={()=>{
+            history.push('/search')
+        }} variant="outlined">
             Explore NearBy
         </Button>
       </div>
